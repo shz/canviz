eb5ff809c997ddacb4febca60b21e89c19fc8a58
Patrick Stein <lylepstein@gmail.com>
2016-07-20 16:33:21 -0700

    Transforms

--- lib/canvas.ts
+++ lib/canvas.ts
@@ -1,5 +1,7 @@
 import AffineMatrix from './affine_matrix';
 
+type CanvasImageSource = HTMLImageElement | HTMLVideoElement | HTMLCanvasElement;
+
 /**
  * Cross-platform method to create a canvas
  */
@@ -41,6 +43,12 @@ export default class Canvas {
    */
   _saveCount: number;
 
+  /**
+   * A matrix equivalent to the CanvasRenderingContext2D's transform
+   * matrix, *without* the device pixel ratio scaling.
+   */
+  _transform: AffineMatrix;
+
   constructor(element?: HTMLCanvasElement) {
     if (element === undefined) {
       element = createCanvas();
@@ -49,6 +57,9 @@ export default class Canvas {
     // Basic setup
     this.el = element;
     this.ctx = element.getContext('2d');
+    this._saveCount = 0;
+    this._ratio = 0;
+    this._transform = new AffineMatrix();
 
     // Rendering hints
     {
@@ -108,6 +119,64 @@ export default class Canvas {
 
     // Re-apply our scaling ratio to keep things sane
     this.ctx.scale(this._ratio, this._ratio);
+
+    // Reset our transform matrix.  Ignore the device pixel ratio and just
+    // use the base scaling.
+    this._transform = new AffineMatrix().scale(this.scaling, this.scaling);
+  }
+
+  /**
+   * Gets the current transform matrix.  The matrix returned here is a
+   * copy, so feel free to go wild and modify it to your heart's content.
+   *
+   * Polyfill, kinda.
+   */
+  get currentTransform(): AffineMatrix {
+    return new AffineMatrix(this._transform);
+  }
+
+  /**
+   * Push state onto the state stack
+   */
+  save() {
+    this._saveCount++;
+    this.ctx.save();
+  }
+
+  /**
+   * Pop state from the state stack
+   */
+  restore() {
+    if (this._saveCount > 0) {
+      this.ctx.restore();
+      this._saveCount--;
+    }
+  }
+
+  // These transform functions are delegated to the underlying CanvasRenderingContext2D,
+  // but we also copy their actions to our stored transform matrix.
+
+  scale(x: number, y: number) {
+    this._transform = this._transform.scale(x, y);
+    this.ctx.scale(x, y);
+  }
+  rotate(rad: number) {
+    this._transform = this._transform.rotate(rad);
+    this.ctx.rotate(rad);
+  }
+  translate(x: number, y: number) {
+    this._transform = this._transform.translate(x, y);
+    this.ctx.translate(x, y);
+  }
+  transform(a: number, b: number, c: number, d: number, e: number, f: number) {
+    this._transform = this._transform.multiply(new AffineMatrix(a, b, c, d, e, f));
+    this.ctx.transform(a, b, c, d, e, f);
+  }
+  setTransform(a: number, b: number, c: number, d: number, e: number, f: number) {
+    this._transform = new AffineMatrix().scale(this.scaling, this.scaling).multiply(
+      new AffineMatrix(a, b, c, d, e, f)
+    );
+    this.ctx.setTransform(a, b, c, d, e, f);
   }
 
   // The rest here is directly delegated to the CanvasRenderingContext2D
@@ -123,5 +192,49 @@ export default class Canvas {
   get strokeStyle(): string | CanvasGradient | CanvasPattern { return this.ctx.strokeStyle; }
   set fillStyle(v: string | CanvasGradient | CanvasPattern) { this.ctx.fillStyle = v; }
   get fillStyle(): string | CanvasGradient | CanvasPattern { return this.ctx.fillStyle; }
+  createLinearGradient(x0: number, y0: number, x1: number, y1: number): CanvasGradient { return this.ctx.createLinearGradient(x0, y0, x1, y1); }
+  createRadialGradient(x0: number, y0: number, r0: number, x1: number, y1: number, r1: number): CanvasGradient { return this.ctx.createRadialGradient(x0, y0, r0, x1, y1, r1); }
+  createPattern(image: CanvasImageSource, repetition?: string): CanvasPattern { return this.ctx.createPattern(image, repetition); }
 
+  // Shadows
+  set shadowOffsetX(v: number) { this.ctx.shadowOffsetX = v; }
+  get shadowOffsetX(): number { return this.ctx.shadowOffsetX; }
+  set shadowOffsetY(v: number) { this.ctx.shadowOffsetY = v; }
+  get shadowOffsetY(): number { return this.ctx.shadowOffsetY; }
+  set shadowColor(v: string) { this.ctx.shadowColor = v; }
+  get shadowColor(): string { return this.ctx.shadowColor; }
+
+  // Rects
+  clearRect(x: number, y: number, w: number, h: number) { this.ctx.clearRect(x, y, w, h); }
+  fillRect(x: number, y: number, w: number, h: number) { this.ctx.fillRect(x, y, w, h); }
+  strokeRect(x: number, y: number, w: number, h: number) { this.ctx.strokeRect(x, y, w, h); }
+
+  // Path API
+  beginPath() { this.ctx.beginPath(); }
+  fill() { this.ctx.fill(); }
+  stroke() { this.ctx.stroke(); }
+  drawFocusIfNeeded(element: HTMLElement) { this.drawFocusIfNeeded(element); }
+  clip() { this.ctx.clip(); }
+  isPointInPath(x: number, y: number): boolean { return this.ctx.isPointInPath(x, y); }
+
+  // Text
+  fillText(text: string, x: number, y: number, maxWidth: number) { this.ctx.fillText(text, x, y, maxWidth); }
+  strokeText(text: string, x: number, y: number, maxWidth: number) { this.ctx.strokeText(text, x, y, maxWidth); }
+  measureText(text: string): TextMetrics { return this.ctx.measureText(text); }
+
+  // Drawing images
+  drawImage(image: CanvasImageSource, offsetX: number, offsetY: number,
+            width?: number, height?: number,
+            canvasOffsetWidth?: number, canvasOffsetHeight?: number,
+            canvasImageWidth?: number, canvasImageHeight?: number) {
+    this.ctx.drawImage(image, offsetX, offsetY,
+                       width, height,
+                       canvasOffsetWidth, canvasOffsetHeight,
+                       canvasImageHeight, canvasImageWidth);
+  }
+
+  // Pixel manipulation
+  createImageData(imageDataOrSw: number | ImageData, sh?: number): ImageData { return this.ctx.createImageData(imageDataOrSw, sh); }
+  getImageData(sx: number, sy: number, sw: number, sh: number): ImageData { return this.ctx.getImageData(sx, sy, sw, sh); }
+  putImageData(imagedata: ImageData, dx: number, dy: number, dirtyX?: number, dirtyY?: number, dirtyWidth?: number, dirtyHeight?: number) { this.ctx.putImageData(imagedata, dx, dy, dirtyX, dirtyY, dirtyWidth, dirtyHeight); }
 }
